275dbe42422a969f08c2224c8bf41d533e16f780
Sabri Allegui <sabri.allegui@vistaprint.com>
2023-01-13 10:45:51 +0100

    checkpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
+import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+
+export class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+
+        <Map google={this.props.google} zoom={14}>
+
+          <Marker onClick={this.onMarkerClick}
+            name={'Current location'} />
+
+          <InfoWindow onClose={this.onInfoWindowClose}>
+            <div>
+              {/* <h1>{this.state.selectedPlace.name}</h1> */}
+            </div>
+          </InfoWindow>
+        </Map>
+
+      </div>
+    )
+  }
 }
 
-export default App;
+
+
+export default GoogleApiWrapper({
+  apiKey: ("AIzaSyDVNp3Ad_CN_8y2GAVG9vhgBqclcqO4i_Q")
+})(App)
+
